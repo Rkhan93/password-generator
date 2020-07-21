e4e0e52f035f93832c1306a9efd660bc245b8e45
Ridge  Khan <ridgekhan@Ridges-MBP.attlocal.net>
2020-07-20 22:09:52 -0500

    changes to hoemework

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # password-generator
+Description: The password is randomly generated based on criteria that the user provides when prompted.
+
+When the user clicks the button "Generate Password" they are first promoted for password length. If outside the range of 8-128, the user is prompted to once again enter a password within the range. 
+
+The program then asks the user to if they would like to include numbers, characters, uppercase and/or lowercase letters. 
+
+When values have been defined, the password will appear within the password box.
--- script.js
+++ script.js
@@ -6,9 +6,45 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  // Arrays
+var numericCharacter = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacter = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+passwordText.value = password;
 
 }
 
+// ALL YOUR CODE FOR THIS HOMEWORK WILL HAPPEN HERE
+function generatePassword(){ 
+
+  //How many characters would you like to enter for your password 
+  
+  var password = prompt("How many characters would you like enter your password?");
+  } 
+
+//if 
+
+if(passwordText <= 8) (passwordText >= 128)
+  alert("Password length must be between 8-128 characters");
+  
+
+  // Details of password 
+  var specialCharacter = confirm("Click OK to confirm if you would like to include special characters");
+  var numericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
+  var lowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
+  var upperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
+    
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Defining a function looks like this:
+  // function putTheNameOfYourFunctionHere () {}
+
+// Defining any other variable looks like this:
+  // var putTheNameOfYourVariableHere;
+
